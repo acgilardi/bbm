b23f66806e68c3abb21d8f8c974ed8b820f69556
Anthony Gilardi <acgilardi@gmail.com>
2015-05-30 17:40:09 -0400

    tag0 - Fix hello world message

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-    response.send('Hello World!');
+    response.send('Thats reDisculous');
 });
 
 app.listen(8080, function() {
