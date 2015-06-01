86627945c73a082f17102c526db3a2e30569be82
Anthony Gilardi <acgilardi@gmail.com>
2015-05-31 21:26:03 -0400

    3 - Bower, bootstrap and serve index.html

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,10 @@
 module.exports = function(grunt) {
     grunt.initConfig({
         watch: {
+            dev: {
+                files: ['src/**/*.*'],
+                tasks: ['build']
+            },
             options: {
                 liveReload: true
             }
@@ -13,13 +17,37 @@ module.exports = function(grunt) {
                     node_env: 'dev'
                 }
             }
+        },
+        bower: {
+            install: {
+                options: {
+                    targetDir: 'src/assets/js/vendor',
+                    layout: 'byComponent'
+                }
+            }
+        },
+        copy: {
+            build: {
+                files: [
+                    {
+                        src: [ '**' ],
+                        dest: 'build/',
+                        cwd: 'src',
+                        expand: true
+                    }
+                ]
+            }
         }
     });
 
     // load plugins
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // define tasks
-    grunt.registerTask('server', ['express:dev', 'watch']);
+    grunt.registerTask('default', ['bower']);
+    grunt.registerTask('build', ['copy:build']);
+    grunt.registerTask('server', ['build', 'express:dev', 'watch']);
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static(__dirname + '/build'));
 app.get('/', function(request, response) {
     response.send('Thats reDisculous');
 });
